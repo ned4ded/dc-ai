3b025fa61b4ac43519c032474d5f2abd219af46a
Eugen Bazhenov <wtf.czabe@gmail.com>
2023-11-23 13:49:39 +0300

    feat: add benchmark, rating and prices datasets

--- hw-1/parser/index.js
+++ hw-1/parser/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable camelcase */
 // @ts-check
@@ -21,6 +22,9 @@ const dirs = {
     CACHE: join(DIRNAME, './cache'),
     RESULT: join(DIRNAME, './data'),
     RESULT_SPECS: join(DIRNAME, './data/techpowerup_gpus_specifications.json'),
+    RESULT_BENCHMARKS: join(DIRNAME, './data/tomshardware_gpus_benchmarks.json'),
+    RESULT_PRICES: join(DIRNAME, './data/pcpartpicker_gpus_prices.json'),
+    RESULT_RATINGS: join(DIRNAME, './data/userbenchmark_gpus_rating.json'),
 };
 
 if (!existsSync(dirs.CACHE)) await fs.mkdir(dirs.CACHE);
@@ -60,11 +64,11 @@ const CACHE = {
 
 const fetchAndSave = async (url) => {
     console.log(`[fetchAndSave] fetching ${url}`);
-    const html = CACHE.getCacheOrNull(url);
+    const html = await CACHE.getCacheOrNull(url);
     if (html) return html;
 
     console.log('[fetchAndSave] loading page');
-    const d = await fetch('https://pcbuilder.net/product/graphics-card/');
+    const d = await fetch(url);
     const text = await d.text();
     CACHE.saveHtmlInCache(url, text);
     console.log('[fetchAndSave] done');
@@ -103,8 +107,14 @@ const html2json = (html) => {
 
 const URLS = {
     TECHPOWERUP_GPUS_SPECS: 'https://www.techpowerup.com/gpu-specs/',
+    TOMSHARDWARE_BENCHMARKS: 'https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html',
+    PCPARTPICKER_GPUS_LIST: 'https://pcpartpicker.com/products/video-card/',
 };
 
+const RESOURSES = {
+    PCPARTPICKER: join(DIRNAME, './resources/pcpartpicker/'),
+    USERBENCHMARK: join(DIRNAME, './resources/userbenchmark/'),
+}
 
 const randomTimeout = (from, to) => new Promise(res => {
     const ms = _.random(from, to, false);
@@ -127,7 +137,26 @@ const scrappers = {
         }
         if (!page) {
             page = await browser.newPage();
-            await page.setViewport({ width: 1080, height: 1024 });
+            await page.setViewport({ width: 1600, height: 900 });
+            await page.setBypassCSP(true);
+            await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0')
+            await page.setCookie({
+                'domain': '.pcpartpicker.com',
+                name: 'cf_clearance',
+                value: 'w.7pzSg0O6YsZpU8l6mhZ.lMCh5HcnSzqiMXElw2glI-1700629269-0-1-79ba6542.83a555d2.2868fde8-160.2.1700629269',
+                httpOnly: true,
+                secure: true,
+                sameSite: "None",
+                path: "/",
+            },
+                {
+                    domain: 'pcpartpicker.com',
+                    name: 'xcsrftoken',
+                    value: '3q0tmDvuAGmtL2TvjgswBRPVV3dR27ybdlBMq60DgK9LBi3uCp4Imq8TpYxTy6qC',
+                    httpOnly: false,
+                    secure: false,
+                    sameSite: "Lax",
+                })
         }
         return page
     },
@@ -187,7 +216,7 @@ const scrappers = {
         CACHE.saveHtmlInCache(url.toString(), theHtml);
         console.log('[getGpusByGeneration]: done')
         return [false, theHtml];
-    }
+    },
 }
 
 const parsers = {
@@ -221,7 +250,7 @@ const parsers = {
                 const [memory_size, memory_type, memory_bus] = parsers.getText(values.get(4)).split(',').map(s => s.trim());
                 return {
                     manufacturer: parsers.getText(headings[i]),
-                    product_name: parsers.getText(values.get(0)),
+                    model_name: parsers.getText(values.get(0)),
                     url: `https://www.techpowerup.com${$(values.get(0)).find('a').attr('href')}`,
                     chip: parsers.getText(values.get(1)),
                     release_date: parse(parsers.getText(values.get(2)), "MMM do, yyyy", new Date()),
@@ -236,6 +265,48 @@ const parsers = {
         })
 
         return _.flatten(tbodies);
+    },
+
+    getFps: (e) => Number(parsers.getText(e)?.match(/\((\d+\.\d+)fps\)/)?.[0]?.match(/\d+\.\d+/)?.[0]),
+
+    getThwBenchmarks: (html) => {
+        const $ = load(html);
+        const $rows = $('#slice-container-table-L8SfcAV5o28ayEFjzq6cfX-13 .table__container tbody > .table__body__row');
+        return $rows.toArray().map(i => {
+            const $d = $(i).find('td');
+
+            return {
+                model_name: parsers.getText($d.get(0)).replace(/[\s\xA0]+/g, ' ').trim(),
+                '1080p_ultra': parsers.getFps($d.get(2)),
+                '1080p_medium': parsers.getFps($d.get(3)),
+                '1440p_ultra': parsers.getFps($d.get(4)),
+                '4k_ultra': parsers.getFps($d.get(5)),
+            }
+        })
+    },
+
+    getPppPrices: (html) => {
+        const $ = load(html);
+        const $rows = $('#category_content > tr');
+        return $rows.toArray().map(element => {
+            const $element = $(element);
+            const product_name = parsers.getText($element.find('.td__name .td__nameWrapper p').text());
+            const model_name = $element.find('.td__spec--1').text().replace('Chipset', '');
+            const price = Number(parsers.getText($element.find('.td__price').get(0)).replace('Add', '').replace(/[^\d.]/g, ''));
+            return { product_name, model_name, price }
+        });
+    },
+
+    getUbmRatings: (html) => {
+        const $ = load(html);
+        const $rows = $('.table.mh-td.table-v-center.table-h-center tbody > tr');
+        return $rows.toArray().map(element => {
+            const $element = $(element);
+            const model_name = parsers.getText($element.find('td').get(2));
+            const rating = Number($element.find('td:nth-child(4) > div:first').text().trim().replace(/[^\d.]/g, ''));
+            const price = Number($element.find('td:nth-child(9) > div:first').text().trim().replace(/[^\d.]/g, ''));
+            return { model_name, rating, price }
+        });
     }
 };
 
@@ -247,7 +318,7 @@ const fetchAndSaveGpuSpecifications = async () => {
     let gpus = [];
     // eslint-disable-next-line no-restricted-syntax
     for (const [index, generation] of gpuGenerationsList.entries()) {
-        
+
         console.log(`[fetchAndSaveGpuSpecifications]: done ${index + 1} / ${gpuGenerationsList.length}`);
         console.log(`[fetchAndSaveGpuSpecifications]: starting to process generation ${generation.value}`);
         console.log(`[fetchAndSaveGpuSpecifications]: expect to receive ${generation.count} rows`);
@@ -268,10 +339,57 @@ const fetchAndSaveGpuSpecifications = async () => {
     console.log(`[script] GPUs specifications has been parsed successfully. Count: ${gpus.length}.`);
 }
 
+const fetchAndSaveGpuBenchmarks = async () => {
+    console.log(`[script] Starting to load GPUs benchmarks.`);
+    const html = await fetchAndSave(URLS.TOMSHARDWARE_BENCHMARKS);
+    const d = parsers.getThwBenchmarks(html);
+
+    await fs.writeFile(dirs.RESULT_BENCHMARKS, JSON.stringify(d, null, 2))
+    console.log(`[script] GPUs benchmarks has been parsed successfully.`);
+}
+
+const readFilesInDirAndExecuteFn = async (dir, fn) => {
+    console.log(`[readFilesInDirAndExecuteFn] Starting to read ${dir}`);
+    const d = await fs.readdir(dir);
+    let result = [];
+    for (const fileName of d) {
+        const content = await fs.readFile(join(dir, fileName));
+        const parsed = fn(content.toString());
+        result = result.concat(parsed);
+    }
+    return result;
+}
+
+const readAndSaveGpuPrices = async () => {
+    // PCPARTPICKER_GPUS_LIST
+    console.log(`[script] Starting to load GPUs prices.`);
+    // @ts-ignore
+    const d = await readFilesInDirAndExecuteFn(RESOURSES.PCPARTPICKER, parsers.getPppPrices);
+    await fs.writeFile(dirs.RESULT_PRICES, JSON.stringify(d, null, 2))
+    console.log(`[script] GPUs prices has been parsed successfully.`);
+}
+
+const readAndSaveGpuRatings = async () => {
+    // PCPARTPICKER_GPUS_LIST
+    console.log(`[script] Starting to load GPUs ratings.`);
+    // @ts-ignore
+    const d = await readFilesInDirAndExecuteFn(RESOURSES.USERBENCHMARK, parsers.getUbmRatings);
+    await fs.writeFile(dirs.RESULT_RATINGS, JSON.stringify(d, null, 2))
+    console.log(`[script] GPUs ratings has been parsed successfully.`);
+}
+
 (async () => {
     // https://www.techpowerup.com/gpu-specs/?mobile=No&igp=No&sort=name
-
     await fetchAndSaveGpuSpecifications();
+
+    // https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html
+    await fetchAndSaveGpuBenchmarks();
+
+    // https://pcpartpicker.com/products/video-card/
+    await readAndSaveGpuPrices();
+
+    // https://gpu.userbenchmark.com/
+    await readAndSaveGpuRatings();
 
     // const url = 'https://pcbuilder.net/product/graphics-card/';
     // const text = await fetchAndSave(url);
